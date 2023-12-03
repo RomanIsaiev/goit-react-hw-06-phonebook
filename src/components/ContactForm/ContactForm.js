08d5f396f76a2588e1d3e9b64edd94f67e8aa61c
Roman Isaiev <roma.isaev.2000@gmail.com>
2023-12-03 14:33:02 +0200

    micro fix
    add return when contacts already in contacts

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,14 +1,28 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/contactsSlice';
+import { getContacts } from 'redux/selectors';
 
 export const ContactForm = () => {
   const dispatch = useDispatch();
+
+  const contacts = useSelector(getContacts);
 
   const handleSubmit = event => {
     event.preventDefault();
 
     const form = event.target;
     const { name, number } = form.elements;
+
+    if (
+      contacts.find(
+        item =>
+          item.name.toLowerCase().trim() === name.value.toLowerCase().trim()
+      )
+    ) {
+      alert(`${name.value} is already in contacts`);
+      return;
+    }
+
     dispatch(addContact(name.value, number.value));
     form.reset();
   };
